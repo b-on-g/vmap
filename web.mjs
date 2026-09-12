a3b81b06ecba0885f07223fb0db884931cb57004
Dev-cmyser <105227884+Dev-cmyser@users.noreply.github.com>
2026-09-12 20:27:42 +0000

    Deploying to gh-pages from @ b-on-g/vmap@25afc0865f51e17ff55720bcd697081980d698e4 🚀

--- web.mjs
+++ web.mjs
@@ -38421,9 +38421,12 @@ var $;
     (function ($$) {
         $mol_style_define($bog_vmap_app_shelf, {
             flex: { basis: '20rem' },
+            Tools: {
+                minWidth: 0,
+                flex: { grow: 1, shrink: 1, basis: '6rem' },
+            },
             Filter: {
                 minWidth: 0,
-                flex: { shrink: 1, basis: '6rem' },
             },
             Item_row: {
                 font: { family: 'inherit', size: '.9rem' },
